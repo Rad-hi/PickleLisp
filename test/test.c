cf4c2ad1bee1609b6c441536a1c914ce1ba229ad
rs <rs@spectre.com>
2024-10-17 00:28:14 +0100

    cleaned up the tests

--- test/test.c
+++ test/test.c
@@ -10,8 +10,8 @@
 #define PRINT_VERDICT(cond, name)   printf("[%s] [Test %s]\n", (cond) ? "PASSED" : "FAILED", (name));
 
 
-static void assert_equal(Lval_t val, Lval_t expected, LVAL_e type, char* test_name) {
-  switch (type) {
+static void assert_equal(Lval_t val, Lval_t expected, char* test_name) {
+  switch (expected.type) {
     case LVAL_ERR: {
       bool cond = val.err == expected.err;
       PRINT_VERDICT(cond, test_name);
@@ -41,6 +41,27 @@ static void assert_equal(Lval_t val, Lval_t expected, LVAL_e type, char* test_na
   }
 }
 
+Lval_t get_lval_err(int value) {
+  return (Lval_t){
+    .type = LVAL_ERR,
+    .err = value
+  };
+}
+
+Lval_t get_lval_double(double value) {
+  return (Lval_t){
+    .type = LVAL_DECIMAL,
+    .num.f_num = value
+  };
+}
+
+Lval_t get_lval_long(long value) {
+  return (Lval_t){
+    .type = LVAL_INTEGER,
+    .num.li_num = value
+  };
+}
+
 /*------------------*/
 /* TESTS START HERE */
 /*------------------*/
@@ -49,21 +70,12 @@ static void test_integer_addition(mpc_parser_t* language) {
 
   char* test_name = "integer addition";
   char* test_statement = "+ 1 1";
-  LVAL_e type = LVAL_INTEGER;
-
-  /////////////////////////////////////////////////////
-  // TODO: make this universal and less prone to errors
-  /////////////////////////////////////////////////////
-
-  Lval_t expected = {
-    .type = type,
-    .num.li_num = 2
-  };
+  Lval_t expected = get_lval_long(2);
 
   mpc_result_t r;
   if (mpc_parse("test", test_statement, language, &r)) {
     Lval_t res = eval_ast(r.output);
-    assert_equal(res, expected, type, test_name);
+    assert_equal(res, expected, test_name);
     mpc_ast_delete(r.output);
   }
 }
@@ -72,16 +84,12 @@ static void test_decimal_addition(mpc_parser_t* language) {
 
   char* test_name = "decimal addition";
   char* test_statement = "+ 1. 3.5";
-  LVAL_e type = LVAL_DECIMAL;
-  Lval_t expected = {
-    .type = type,
-    .num.f_num = 4.5
-  };
+  Lval_t expected = get_lval_double(4.5);
 
   mpc_result_t r;
   if (mpc_parse("test", test_statement, language, &r)) {
     Lval_t res = eval_ast(r.output);
-    assert_equal(res, expected, type, test_name);
+    assert_equal(res, expected, test_name);
     mpc_ast_delete(r.output);
   }
 }
@@ -90,16 +98,12 @@ static void test_heterogenous_addition(mpc_parser_t* language) {
 
   char* test_name = "heterogenous addition";
   char* test_statement = "+ 32 33. 2.4 .6 1";
-  LVAL_e type = LVAL_DECIMAL;
-  Lval_t expected = {
-    .type = type,
-    .num.f_num = 69.
-  };
+  Lval_t expected = get_lval_double(69.);
 
   mpc_result_t r;
   if (mpc_parse("test", test_statement, language, &r)) {
     Lval_t res = eval_ast(r.output);
-    assert_equal(res, expected, type, test_name);
+    assert_equal(res, expected, test_name);
     mpc_ast_delete(r.output);
   }
 }
@@ -108,16 +112,12 @@ static void test_all_operators(mpc_parser_t* language) {
 
   char* test_name = "all operators";
   char* test_statement = "+ 1 (* 1 2) (/ 4 2) (% 15 2) (- 0 6) (^ 3 2)";
-  LVAL_e type = LVAL_INTEGER;
-  Lval_t expected = {
-    .type = type,
-    .num.li_num = 9
-  };
+  Lval_t expected = get_lval_long(9);
 
   mpc_result_t r;
   if (mpc_parse("test", test_statement, language, &r)) {
     Lval_t res = eval_ast(r.output);
-    assert_equal(res, expected, type, test_name);
+    assert_equal(res, expected, test_name);
     mpc_ast_delete(r.output);
   }
 }
@@ -126,16 +126,12 @@ static void test_min_max(mpc_parser_t* language) {
 
   char* test_name = "min max";
   char* test_statement = "min 69 (max 666 420)";
-  LVAL_e type = LVAL_INTEGER;
-  Lval_t expected = {
-    .type = type,
-    .num.li_num = 69
-  };
+  Lval_t expected = get_lval_long(69);
 
   mpc_result_t r;
   if (mpc_parse("test", test_statement, language, &r)) {
     Lval_t res = eval_ast(r.output);
-    assert_equal(res, expected, type, test_name);
+    assert_equal(res, expected, test_name);
     mpc_ast_delete(r.output);
   }
 }
